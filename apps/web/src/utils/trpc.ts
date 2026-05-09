64643a8ca9ff317802b5c99a25f70554b7d4dd75
urdadx <nerdyshinobi12@gmail.com>
2026-05-09 06:02:00 +0000

    initial commit

---
+++ apps/web/src/utils/trpc.ts
@@ -0,0 +1,4 @@
+import type { AppRouter } from "@better-comments/api/routers/index";
+import { createTRPCContext } from "@trpc/tanstack-react-query";
+
+export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<AppRouter>();
---
+++ packages/db/src/index.ts
@@ -0,0 +1,15 @@
+import { env } from "@better-comments/env/server";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  const client = createClient({
+    url: env.DATABASE_URL,
+  });
+
+  return drizzle({ client, schema });
+}
+
+export const db = createDb();
