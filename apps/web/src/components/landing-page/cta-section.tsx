363b81ee39ef20e1ca918f08ba2d3b2d784bc6a4
urdadx <nerdyshinobi12@gmail.com>
2026-07-05 22:33:14 +0000

    minor changes

--- apps/web/src/components/landing-page/cta-section.tsx
+++ apps/web/src/components/landing-page/cta-section.tsx
@@ -10,15 +10,16 @@ export const CTASession = () => {
           <div className="mx-auto max-w-7xl px-6">
             <div className="max-w-3xl  text-center sm:mx-auto lg:mr-auto lg:mt-0 lg:w-4/5">
               <h1 className="mt-8 text-balance text-4xl font-semibold md:text-5xl xl:text-6xl xl:leading-[1.3] instrument-serif-regular">
-                Better audience engagement for your{" "}
+                Beautiful comment section for your{" "}
                 <span className="text-primary bg-primary/10 px-1 rounded relative inline-block z-1">
-                  content
+                  blog
                 </span>
               </h1>
 
               <p className="mx-auto mt-8 hidden max-w-xl text-wrap text-lg sm:block">
-                Bizme helps you create engaged communities by artfully promoting
-                your best content and carefully fueling the best conversations.
+                Bizme gives you a complete engagement infrastructure for
+                building and deploying beautiful comment and polls for your
+                blog.
               </p>
               <p className="mx-auto mt-6 block max-w-xl text-wrap text-lg sm:hidden"></p>
             </div>
