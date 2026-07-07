97f867860fe383946f8a1e58f4258f0f9ebc578c
urdadx <nerdyshinobi12@gmail.com>
2026-07-07 11:10:29 +0000

    removed domains for now

--- apps/web/src/components/app-sidebar.tsx
+++ apps/web/src/components/app-sidebar.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarHeader,
-	SidebarInset,
-	SidebarProvider,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarInset,
+  SidebarProvider,
 } from "@/components/ui/sidebar";
 import { Outlet } from "@tanstack/react-router";
 import { Navbar } from "./navbar";
@@ -25,81 +25,81 @@ import { NavActivity } from "./nav-activity";
 import { GlobeLinear } from "@/assets/icons/globe-icon";
 
 const data = {
-	overview: [
-		{
-			title: "Overview",
-			url: "/overview",
-			icon: HomeLinear,
-		},
-	],
+  overview: [
+    {
+      title: "Overview",
+      url: "/overview",
+      icon: HomeLinear,
+    },
+  ],
 
-	activity: [
-		// {
-		// 	title: "Pages",
-		// 	url: "/pages",
-		// 	icon: WindowIcon,
-		// },
-		{
-			title: "Comments",
-			url: "/comments",
-			icon: MessageIcon,
-		},
-		{
-			title: "Polls",
-			url: "/polls",
-			icon: ChatLogsIcon,
-		},
-	],
-	customize: [
-		{
-			title: "Customize",
-			url: "/customize",
-			icon: PaletteRoundLinear,
-		},
-	],
-	settings: [
-		{
-			title: "Analytics",
-			url: "/analytics",
-			icon: ChartLinear,
-		},
-		{
-			title: "Settings",
-			url: "/settings",
-			icon: SettingsLinear,
-		},
+  activity: [
+    // {
+    // 	title: "Pages",
+    // 	url: "/pages",
+    // 	icon: WindowIcon,
+    // },
+    {
+      title: "Comments",
+      url: "/comments",
+      icon: MessageIcon,
+    },
+    {
+      title: "Polls",
+      url: "/polls",
+      icon: ChatLogsIcon,
+    },
+  ],
+  customize: [
+    {
+      title: "Customize",
+      url: "/customize",
+      icon: PaletteRoundLinear,
+    },
+  ],
+  settings: [
+    {
+      title: "Analytics",
+      url: "/analytics",
+      icon: ChartLinear,
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: SettingsLinear,
+    },
 
-		{
-			title: "Domain",
-			url: "/domain",
-			icon: GlobeLinear,
-		},
-	],
+    // {
+    // 	title: "Domain",
+    // 	url: "/domain",
+    // 	icon: GlobeLinear,
+    // },
+  ],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	return (
-		<SidebarProvider>
-			<Sidebar collapsible="icon" className="bg-[#FCFCFC]" {...props}>
-				<SidebarHeader>
-					<SiteSwitcher />
-				</SidebarHeader>
-				<SidebarContent className="bg-[#FCFCFC]">
-					<NavMain items={data.overview} />
-					<NavCustomize items={data.customize} />
-					<NavActivity items={data.activity} />
-					<NavSettings items={data.settings} />
-				</SidebarContent>
-				<SidebarFooter>
-					<NavUser />
-				</SidebarFooter>
-			</Sidebar>
-			<SidebarInset className="min-h-0 min-w-0 flex flex-col overflow-x-hidden">
-				<Navbar />
-				<div className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden">
-					<Outlet />
-				</div>
-			</SidebarInset>
-		</SidebarProvider>
-	);
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible="icon" className="bg-[#FCFCFC]" {...props}>
+        <SidebarHeader>
+          <SiteSwitcher />
+        </SidebarHeader>
+        <SidebarContent className="bg-[#FCFCFC]">
+          <NavMain items={data.overview} />
+          <NavCustomize items={data.customize} />
+          <NavActivity items={data.activity} />
+          <NavSettings items={data.settings} />
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser />
+        </SidebarFooter>
+      </Sidebar>
+      <SidebarInset className="min-h-0 min-w-0 flex flex-col overflow-x-hidden">
+        <Navbar />
+        <div className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
